1cceba72a14665cc68e1f93ea3071a5c0dffffa9
lichu <1371772034@qq.com>
2018-05-06 23:13:40 +0800

    py

--- Problems/79WordSearch.py
+++ Problems/79WordSearch.py
@@ -1,50 +1,46 @@
-board =[
-  ['A','B','C','E'],
-  ['S','F','C','S'],
-  ['A','D','E','E']
-]
+class Solution:
+    def exist(self,board,word):
+        index = 0
+        arr = []
+        m = len(board)
+        n = len(board[0])
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[index]:
+                    arr.append((i,j))
+        index += 1
+        for i,j in arr:
+            visited = []
+            visited.append((i,j))
+            if self.find(board,word,i,j,index,visited):
+                return True
+        return False
 
-word = 'SEEHHHH'
+    def is_legal(self,board,i,j):
+        m = len(board)
+        n = len(board[0])
+        return i >= 0 and i<m and j>=0 and j<n
 
-arr = [1,2]
-val = arr.pop(0)
 
-def find(board,word):
-	queue = []
-	index = 0
-	m = len(board)
-	n = len(board[0])
-	visited = set()
-	for i in range(m):
-		for j in range(n):
-			if board[i][j] == word[index]:
-				if (i,j) not in visited:
-					queue.append((i,j))
-					visited.add((i,j))
-	while len(queue)>0:
-		i,j = queue.pop(0)
-		index += 1
-		if index == len(word):
-			break
-		char = word[index]
-		search(queue,visited,board,i,j,char)
-	return index == len(word)
+    def get_arround(self,board,i,j,char,visited):
+        ret_arr = []
+        for k in [-1,1]:
+            if  self.is_legal(board,i,j+k) and (i,j+k) not in visited and board[i][j+k] == char:
+                ret_arr.append((i,j+k))
+            if  self.is_legal(board,i+k,j) and (i+k,j) not in visited and board[i+k][j] == char:
+                ret_arr.append((i+k,j))
+        return ret_arr
 
-def search(queue,visited,board,i,j,char):
-	m = len(board)
-	n = len(board[0])
-	for k in [-1,1]:
-		if i+k>=0 and i+k<m:
-			if board[i+k][j] == char:
-				if (i+k,j) not in visited:
-					queue.append((i+k,j))
-					visited.add((i+k,j))
-		if j+k>=0 and j+k<n:
-			if board[i][j+k] == char:
-				if (i,j+k) not in visited:
-					queue.append((i,j+k))
-					visited.add((i,j+k))
 
-isTrue = find(board,word)
-print(isTrue)
-
+    def find(self,board,word,i,j,word_index,visited):
+        if len(word) == word_index:
+            return True
+        around_arr = self.get_arround(board,i,j,word[word_index],visited)
+        if len(around_arr) == 0:
+            return False
+        for i1,j1 in around_arr:
+            visited.append((i1,j1))
+            if self.find(board,word,i1,j1,word_index+1,visited):
+                return True
+            visited.pop()
+        return False
